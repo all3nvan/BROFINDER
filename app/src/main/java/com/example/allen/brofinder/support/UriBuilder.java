1923124593e1429767aa80586589697b43c9a0f9
Bryant Pham <Bryant.Pham@parivedasolutions.com>
2015-04-04 13:05:26 -0500

    Added search user path

--- app/src/main/java/com/example/allen/brofinder/support/UriBuilder.java
+++ app/src/main/java/com/example/allen/brofinder/support/UriBuilder.java
@@ -7,7 +7,7 @@ public final class UriBuilder {
     private static final String AUTHORITY = "104.131.112.87:3000";
 
     private static final String REGISTER_PATH = "users";
-    private static final String FRIEND_PATH = "relationships";
+    private static final String SEARCH_PATH = "search";
     private static final String LOCATION_SESSION_PATH = "locations";
 
     public static String generateRegisterPath() {
@@ -17,10 +17,10 @@ public final class UriBuilder {
                 .toString();
     }
 
-    public static String generateFindFriendsPath() {
+    public static String generateSearchUserPath() {
         return generateBaseUri()
-                .appendPath(FRIEND_PATH)
-                .appendEncodedPath("all")
+                .appendPath(REGISTER_PATH)
+                .appendPath(SEARCH_PATH)
                 .build()
                 .toString();
     }
