60faa8529826983c18c9a084864edc0442ff9070
Daniel Dao <daniel.vu.dao@gmail.com>
2015-04-03 22:47:00 -0500

    made location session class

---
+++ app/src/main/java/com/example/allen/brofinder/support/LocationSessionItemArrayAdapter.java
@@ -0,0 +1,12 @@
+package com.example.allen.brofinder.support;
+
+
+import android.widget.ArrayAdapter;
+
+import com.example.allen.brofinder.domain.LocationSession;
+
+public class LocationSessionItemArrayAdapter extends ArrayAdapter<LocationSession>{
+
+
+
+}
